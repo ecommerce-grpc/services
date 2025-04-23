a69913b4e5b448aec66e1a94eee6285a94352697
Marc Pires <marcpiresrj@gmail.com>
2025-04-23 16:48:37 +0000

    feat: Add adapter

---
+++ order/internal/adapters/db/db.go
@@ -0,0 +1,90 @@
+// Package defines the concrete implementation of the DBPort interface
+package db
+
+import (
+	"fmt"
+
+	"github.com/marcpires/grpc/ecommerce/order/internal/application/core/domain"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// Order defines a customer order model
+type Order struct {
+	gorm.Model
+	CustomerID int64
+	Status     string
+	OrderItems []OrderItem
+}
+
+// OrderItem defines a model with the items purchased by a customer
+type OrderItem struct {
+	gorm.Model
+	ProductCode string
+	UnitPrice   float32
+	Quantity    int32
+	OrderID     uint
+}
+
+// Adapter defines the database adapter for the application
+type Adapter struct {
+	db *gorm.DB
+}
+
+// NewAdapter returns the concrete implementation of a database adapter or an error
+func NewAdapter(dataSourceUrl string) (*Adapter, error) {
+	db, openErr := gorm.Open(mysql.Open(dataSourceUrl), &gormConfig{})
+	if openErr != nil {
+		return nil, fmt.Errorf("database connection error: %w", openErr)
+	}
+	err := db.AutoMigrate(&Order{}, OrderItem{})
+	if err != nil {
+		return nil, fmt.Errorf("dabatase migration error: %w", err)
+	}
+	return &Adapter{db: db}, nil
+}
+
+func (a Adapter) Get(id string) (domain.Order, error) {
+	var orderEntity Order
+	res := a.db.First(&orderEntity, id)
+	var orderItems []domain.OrderItem
+	for _, orderItem := range orderEntity.OrderItems {
+		orderItems = append(orderItems, domain.OrderItem{
+			ProductCode: orderItem.ProductCode,
+			UnitPrice:   orderItem.UnitPrice,
+			Quantity:    orderItem.Quantity,
+		})
+	}
+	order := domain.Order{
+		ID:         int64(orderEntity.ID),
+		CustomerID: orderEntity.CustomerID,
+		Status:     orderEntity.Status,
+		OrderItems: orderItems,
+		CreateAt:   orderEntity.CreatedAt.UnixNano(),
+	}
+	return order, res.Error
+}
+
+// Save save an order transaction and returns an error if applicable
+func (a Adapter) Save(order *domain.Order) error {
+	var orderItems []OrderItem
+	for _, orderItem := range order.OrderItems {
+		orderItems = append(orderItems, OrderItem{
+			ProductCode: orderItem.ProductCode,
+			UnitPrice:   orderItem.UnitPrice,
+			Quantity:    orderItem.Quantity,
+		})
+	}
+	orderModel := Order{
+		CustomerID: order.CustomerID,
+		Status:     order.Status,
+		OrderItems: orderItems,
+	}
+	// Insert value into db
+	res := a.db.Create(&orderModel)
+	if res.Error == nil {
+		order.ID = int64(orderModel.ID)
+	}
+	return res.Error
+
+}
