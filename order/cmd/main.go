146d655bcb43ee58af7f554717259b1e27e3cac3
Marc Pires <marcpiresrj@gmail.com>
2025-04-24 18:59:49 +0000

    feat: Add program

---
+++ order/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/marcpires/grpc/ecommerce/order/config"
+	"github.com/marcpires/grpc/ecommerce/order/internal/adapters/db"
+	"github.com/marcpires/grpc/ecommerce/order/internal/adapters/grpc"
+	"github.com/marcpires/grpc/ecommerce/order/internal/application/core/api"
+)
+
+func main() {
+  dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
+  if err != nil {
+    log.Fatalf("Failed to connect to database. Error: %v", err)
+  }
+
+  app := api.NewApplication(dbAdapter)
+  grpcAdapter := grpc.NewAdapter(app, config.GetApplicationPort())
+  grpcAdapter.Run()
+}
