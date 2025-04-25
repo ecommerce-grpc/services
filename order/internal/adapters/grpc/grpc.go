d27a44b53e129be59843ad406dd85ea793c49e96
Marc Pires <marcpiresrj@gmail.com>
2025-04-25 14:45:00 +0000

    feat: Update adapter to handle requests

--- order/internal/adapters/grpc/grpc.go
+++ order/internal/adapters/grpc/grpc.go
@@ -7,22 +7,23 @@ import (
 	"github.com/marcpires/grpc/ecommerce/order/internal/application/core/domain"
 )
 
-func (a Adapter) Create(cxt context.Context, 
+// Create handles the Order request endpoint
+func (a Adapter) Create(cxt context.Context,
 	request *order.CreateOrderRequest) (*order.CreateOrderResponse, error) {
-		var orderItems []domain.OrderItem
-		for _, orderItem := range request.OrderItems {
-			orderItem = append(orderItems, domain.OrderItem{
-				ProductCode: orderItem.ProductCode,
-				UnitPrice: orderItem.UnitPrice,
-				Quantity: orderItem.Quantity,
-			})
-		}
-
-		newOrder := domain.NewOrder(request.UserId, orderItems)
-		result, err := a.api.PlaceOrder(newOrder)
-		if err != nil {
-			return nil, err
-		}
-
-		return &order.CreateOrderResponse{OrderId: result.ID}, nil
+	var orderItems []domain.OrderItem
+	for _, orderItem := range request.OrderItems {
+		orderItem = append(orderItems, domain.OrderItem{
+			ProductCode: orderItem.ProductCode,
+			UnitPrice:   orderItem.UnitPrice,
+			Quantity:    orderItem.Quantity,
+		})
 	}
+
+	newOrder := domain.NewOrder(request.UserId, orderItems)
+	result, err := a.api.PlaceOrder(newOrder)
+	if err != nil {
+		return nil, err
+	}
+
+	return &order.CreateOrderResponse{OrderId: result.ID}, nil
+}
