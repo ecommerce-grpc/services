30255af98e3d08fcf158a01d09a9ca42cdda10a8
Marc Pires <marcpiresrj@gmail.com>
2025-05-01 19:46:12 +0000

    feat: Add better error handling

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -3,11 +3,14 @@ package api
 import (
 	"github.com/marcpires/grpc/ecommerce/order/internal/application/core/domain"
 	"github.com/marcpires/grpc/ecommerce/order/internal/ports"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // Application defines the application.
 type Application struct {
-	db ports.DBPort
+	db      ports.DBPort
 	payment ports.PaymentPort
 }
 
@@ -24,9 +27,21 @@ func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	if err != nil {
 		return domain.Order{}, err
 	}
+	
 	paymentErr := a.payment.Charge(&order)
-	if err != nil {
-		return domain.Order{}, paymentErr
+	// Better error handling with more detailed errors
+	if paymentErr != nil {
+		st, _ := status.FromError(paymentErr)
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field: "payment",
+			Description: st.Message(),
+			Reason: "PAYMENT_INVALID_ARGUMENT",
+		}
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+		statusWithDetails, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDetails.Err()
 	}
 	return order, nil
 }
