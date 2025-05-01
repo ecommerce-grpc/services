ac7daa712906dedc40ddea7c60d9d24419fa996d
Marc Pires <marcpiresrj@gmail.com>
2025-05-01 18:21:17 +0000

    feat: Update implementation

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -8,10 +8,11 @@ import (
 // Application defines the application.
 type Application struct {
 	db ports.DBPort
+	payment ports.PaymentPort
 }
 
 // NewApplication return an Application pointer with a database dependency.
-func NewApplication(db ports.DBPort) *Application {
+func NewApplication(db ports.DBPort, payment ports.PaymentPort) *Application {
 	return &Application{
 		db: db,
 	}
@@ -22,6 +23,10 @@ func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	err := a.db.Save(&order)
 	if err != nil {
 		return domain.Order{}, err
+	}
+	paymentErr := a.payment.Charge(&order)
+	if err != nil {
+		return domain.Order{}, paymentErr
 	}
 	return order, nil
 }
